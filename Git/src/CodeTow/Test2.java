da50d2024d1a84caaf683123f83b6fd8042dd756
haungdeyi <49771128+haungdeyi@users.noreply.github.com>
2019-04-20 19:45:50 +0800

    在远程端更新(20190420 19:45)

--- Git/src/CodeTow/Test2.java
+++ Git/src/CodeTow/Test2.java
@@ -3,6 +3,5 @@ package CodeTow;
 public class Test2 {
 
 	  private String goodBye = "say goodbye";
-	  private String update = "update in local";
-	  private String updateAgain = "update in local again";
+	  private String update = "update in local;
 }
