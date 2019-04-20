92080e9087aba02a29a706bd011ea6cf5f7e3f93
haungdeyi <49771128+haungdeyi@users.noreply.github.com>
2019-04-20 21:44:56 +0800

    在远程端更新(20190420 21:44)

--- Git/src/CodeOne/Test1.java
+++ Git/src/CodeOne/Test1.java
@@ -5,5 +5,4 @@ public class Test1 {
 	private String hello = "say hello";
         private String update = "update in Git";
 	private String updateBeforLocalAddToIndex = "update in Git befor local add to index";
-	private String updateBeforLocalSubmit = "update in Git befor local submit";
 }
