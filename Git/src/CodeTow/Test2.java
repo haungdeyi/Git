f9e952e89b191e5fb9134fc2eee3d8dca2493c8b
huangdeyi <1534527968@qq.com>
2019-04-20 22:22:48 +0800

    在本地更新(20190420 22:22)

--- Git/src/CodeTow/Test2.java
+++ Git/src/CodeTow/Test2.java
@@ -4,5 +4,5 @@ public class Test2 {
 
 	  private String goodBye = "say goodbye";
 	  private String update = "update in local again";
-	  private String try = "try to change";
+	  private String haha = "try to change";
 }
