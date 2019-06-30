65b5bb8d31de3ad8ce7a893b5482b7733edc4d46
haungdeyi <1534527968@qq.com>
2019-06-30 16:09:46 +0800

    在本地修改于 2019-06-30 16:09

--- Git/src/CodeTow/Test2.java
+++ Git/src/CodeTow/Test2.java
@@ -6,4 +6,6 @@ public class Test2 {
 	  private String update = "update in local again";
 	  private String haha = "try to change";
 	  private String validate = "validate comflict in local";
+	  private String push1 = "1¡¢add to index 2¡¢commit to local 3¡¢push to remote repository";
+	  private String push2 = "team--¡·commit and push";
 }
