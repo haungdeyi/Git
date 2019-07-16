7ca26a43e0e434db36e22288dde1a5479c56f3bf
haungdeyi <1534527968@qq.com>
2019-07-16 22:53:07 +0800

    先提交到本地

--- Git/src/CodeOne/Test1.java
+++ Git/src/CodeOne/Test1.java
@@ -1,7 +1,7 @@
 package CodeOne;
 
 public class Test1 {
-	private String update__remote = "update in remote";
+	private String updateremote = "update in remote";
 	private String hello = "say hello";
     private String update_local = "update in local";    
 }
