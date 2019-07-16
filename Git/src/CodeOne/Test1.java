b80e5a46924411e700ad4a21abb5148131293a06
haungdeyi <49771128+haungdeyi@users.noreply.github.com>
2019-07-16 21:48:08 +0800

    Update Test1.java
    初始化

--- Git/src/CodeOne/Test1.java
+++ Git/src/CodeOne/Test1.java
@@ -3,11 +3,6 @@ package CodeOne;
 public class Test1 {
 	
 	private String hello = "say hello";
-        private String update = "update in Git";
-	private String updateBeforLocalAddToIndex = "update in Git befor local add to index again";
-	private String updateBeforLocalSaySorry = "you are wecome";
-        //update in master
-	    //update in local and push to master
-	    //update in local to make conflict
-	    //update in master to make conflict,this is correctly for myself
+
+        
 }
