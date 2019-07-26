a75fe2f180da45fff6341243f0e5118f4fc974d5
huangdeyi <huangdeyi@zsoft.com.cn>
2019-07-26 10:41:49 +0800

    多个文件冲突，先提交到本地

--- Git/src/CodeTow/Test2.java
+++ Git/src/CodeTow/Test2.java
@@ -7,5 +7,5 @@ public class Test2 {
 	  private String haha = "try to change";
 	  private String validate = "validate comflict in local";
 	  private String push1 = "1¡¢add to index 2¡¢commit to local 3¡¢push to remote repository";
-	  private String push2 = "team--¡·commit and push";
+	  private String push2 = "team--¡·commit and push if not conflict";
 }
