1f8a4d22640f17f39443d2474d1f99279f2301fe
haungdeyi <49771128+haungdeyi@users.noreply.github.com>
2019-07-26 11:40:53 +0800

    更新remote again

--- Git/src/CodeTow/Test2.java
+++ Git/src/CodeTow/Test2.java
@@ -6,6 +6,6 @@ public class Test2 {
 	  private String update = "update in local again";
 	  private String validate = "validate comflict in local";
 	  private String push1 = "1 add to index 2 commit to local 3 push to remote repository";
-	  private String push2 = "team-->commit and push if not conflict";
+	  private String push2 = "team-->commit and push if not conflict....";
 
 }
