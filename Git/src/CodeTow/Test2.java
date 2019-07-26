4ac9f4e6c959d6bedf61f7392c188a51a6f34817
huangdeyi <huangdeyi@zsoft.com.cn>
2019-07-26 11:08:29 +0800

    出现冲突

--- Git/src/CodeTow/Test2.java
+++ Git/src/CodeTow/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	  private String goodBye = "say goodbye";
 	  private String update = "update in local again";
-	  private String haha = "try to change";
+	  private String haha = "try to change what happend??";
 	  private String validate = "validate comflict in local";
 	  private String push1 = "1¡¢add to index 2¡¢commit to local 3¡¢push to remote repository";
 	  private String push2 = "team--¡·commit and push if not conflict";
