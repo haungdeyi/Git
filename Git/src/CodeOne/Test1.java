0af84b48a5acfc3861a48197fbd9df861df62f28
huangdeyi <huangdeyi@zsoft.com.cn>
2019-07-26 11:41:29 +0800

    多个文件冲突

--- Git/src/CodeOne/Test1.java
+++ Git/src/CodeOne/Test1.java
@@ -6,5 +6,5 @@ public class Test1 {
 	private String update_remote = "update in remote!";
 	private String hello = "say hello";
     private String update_local = "update in local aa and add something ib remote";    
-    private String local ="conflict in different area";
+    private String local ="conflict in different area ...";
 }
